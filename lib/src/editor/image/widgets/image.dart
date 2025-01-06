e6207456208b9a18e649e5cff2308d11433b76fa
nizkl89 <112047571+nizkl89@users.noreply.github.com>
2025-01-06 11:15:38 +0800

    Update image.dart

--- lib/src/editor/image/widgets/image.dart
+++ lib/src/editor/image/widgets/image.dart
@@ -83,6 +83,17 @@ Image getImageWidgetByImageSource(
     height: height,
     alignment: alignment,
     errorBuilder: imageErrorWidgetBuilder,
+    loadingBuilder: (context, child, loadingProgress) {
+      if (loadingProgress == null) return child;
+      return Center(
+        child: CircularProgressIndicator(
+          value: loadingProgress.expectedTotalBytes != null
+              ? loadingProgress.cumulativeBytesLoaded /
+                  loadingProgress.expectedTotalBytes!
+              : null,
+        ),
+      );
+    },
   );
 }
 
